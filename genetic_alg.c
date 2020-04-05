ff302c493379ac32c5dc6f8c88f130c761f5b849
golovnyakpa <pgolovnyak@gmail.com>
2020-04-05 21:28:11 +0300

    genetic alg with roulette wheel

--- genetic_alg.c
+++ genetic_alg.c
@@ -19,6 +19,9 @@ const uint64_t zero_and_one_vecs[64] = {0x1,0x3,0x7,0xf,0x1f,0x3f,0x7f,0xff, 0x1
 	                        0x3ffffffffffffff,0x7ffffffffffffff,0xfffffffffffffff,0x1fffffffffffffff,0x3fffffffffffffff,
 	                        0x7fffffffffffffff,0xffffffffffffffff};
 
+const float lower_bound = -20;
+const float upper_bound = -3.1;
+
 
 typedef struct individual
 {
@@ -27,15 +30,18 @@ typedef struct individual
 	float loss_function_value;
 } individual;
 
+float int_to_float(int x, int bits_num){
+	return lower_bound + x*((upper_bound-lower_bound) / (pow(2, bits_num) - 1));
+}
 
-void create_population(int n, individual* population, int lower_bound, int upper_bound){
+void create_population(int n, individual* population, int low, int up){
     int i;
     srand(time(NULL));
     for(i=0; i<n; i++)
-    	population[i].value = lower_bound + rand() % (upper_bound - lower_bound + 1);
+    	population[i].value = low + rand() % (up - low + 1);
 }
 
-int define_number_of_bits(int lower_bound, int upper_bound, int accuracy){
+int define_number_of_bits(int accuracy){
 	int interval = (upper_bound - lower_bound) * pow(10, accuracy);
 	int i = 1;
 	while (pow(2, i) - interval < 0){
@@ -45,7 +51,7 @@ int define_number_of_bits(int lower_bound, int upper_bound, int accuracy){
 }
 
 float f(int x){
-	return sin(2 * x) / sqrt(x);
+	return sin(2 * x) / (x * x);
 }
 
 float sum_value_of_all_fitnesses(individual* population, int n){
@@ -80,18 +86,18 @@ int roulette_wheel_selection(individual* population, int n){
 }
 
 
-float calculte_loss_function_for_individuals(individual* population, int n){
+float calculte_loss_function_for_individuals(individual* population, int n, int bits_num){
 	/*
 	Функция возвращает минимальное значение функции для текущей популяции. В дальнейшем это 
 	значение будет использовано для того, чтобы отнормировать (сделать положительными) все значения 
 	фитнесс функций. Это необходимо для того, чтобы вычислить вероятности попадения хромосомы в 
 	промежуточную популяцию.
 	*/
-	population[0].loss_function_value = f(population[0].value);
-	float min_f_value = population[0].loss_function_value = f(population[0].value);
+	population[0].loss_function_value = f(int_to_float(population[0].value, bits_num));
+	float min_f_value = population[0].loss_function_value;
 	for(int i=1; i<n; i++)
 	{
-		population[i].loss_function_value = f(population[i].value);
+		population[i].loss_function_value = f(int_to_float(population[i].value, bits_num));
 		if (population[i].loss_function_value < min_f_value)
 			min_f_value = population[i].loss_function_value;
 	}
@@ -163,7 +169,15 @@ void mutation(individual* intermidiate_population,int n, int bits_num){
 	}
 }
 
-
+float get_max(individual* population, int n, int bits_num){
+	float max = population[0].loss_function_value;
+	for(int i=0; i<n; i++)
+	{
+		if (population[i].loss_function_value>max)
+			max = population[i].loss_function_value;
+	}
+	return max;
+}
 
 // void intermidiate_population_to_new_population(individual* population, individual* intermidiate_population, int n, float min_f_value){
 // 	// Промежуточная популяция становится новой. Также происходит денормалзация, т.е. от всех 
@@ -178,34 +192,44 @@ void mutation(individual* intermidiate_population,int n, int bits_num){
 
 
 int main(){
-	int n = 20;
-	int bits_num = define_number_of_bits(-20, -3.1, 3);
+	int n = 500;
+	int bits_num = define_number_of_bits(3);
 	int cuts_num = pow(2, bits_num);
 	individual *population = (individual*) malloc(n * sizeof(individual));
 	individual *intermidiate_population = (individual*) malloc(n * sizeof(individual));
     create_population(n, population, 0, cuts_num);
+    for(int j=0; j<30;j++)
+    {
+    	 create_population(n, population, 0, cuts_num);
+	    for(int i=0; i<1000; i++)
+	    {
+	    	// начало репродукции
+			float min_f_value = calculte_loss_function_for_individuals(population, n, bits_num);
+			//printf("%f\n", int_to_float(population[5].loss_function_value, bits_num));
+			if (min_f_value < 0)
+				normalize_all_f_values(population, n, min_f_value);
+			float sum = sum_value_of_all_fitnesses(population, n);
+			define_probabilities(population, n, sum);
+			create_intermidiate_population(population, n, intermidiate_population);
 
-    // начало репродукции
-	float min_f_value = calculte_loss_function_for_individuals(population, n);
-	if (min_f_value < 0)
-		normalize_all_f_values(population, n, min_f_value);
-	float sum = sum_value_of_all_fitnesses(population, n);
-	define_probabilities(population, n, sum);
-	create_intermidiate_population(population, n, intermidiate_population);
-
-	/*
-	Стадия репродукции окончена. Далее следуют стадии кроссинговера
-	и мутации 
-	*/
-	crossingover(intermidiate_population, n, bits_num);
-	mutation(intermidiate_population, n, bits_num);
-	if (min_f_value > 0)
-		min_f_value = 0;
-	individual *temp = intermidiate_population;
-	intermidiate_population = population;
-	population = temp;
-	if (min_f_value < 0)
-		denormalize_all_f_values(population, n, min_f_value);
+			/*
+			Стадия репродукции окончена. Далее следуют стадии кроссинговера
+			и мутации 
+			*/
+			crossingover(intermidiate_population, n, bits_num);
+			mutation(intermidiate_population, n, bits_num);
+			if (min_f_value > 0)
+				min_f_value = 0;
+			individual *temp = intermidiate_population;
+			intermidiate_population = population;
+			population = temp;
+			if (min_f_value < 0)
+				denormalize_all_f_values(population, n, min_f_value);
+		}
+    
+		float res = get_max(population, n, bits_num);
+		printf("%f\n", res);
+    }
 	//ntermidiate_population_to_new_population(population, intermidiate_population, n, min_f_value);
 	//
     //for(int i=0; i<n; i++)
